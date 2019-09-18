3b853cf5cd631e4057d2342e45aff166d1ad80c2
Armend Ukehaxhaj <armendd.u@hotmail.com>
2019-09-18 17:00:37 +0200

    Convolution takes the image's width and height

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 import sys
 
 __author__ = "Armend Ukehaxhaj"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __license__ = "MIT"
 
 from logzero import logger
@@ -14,7 +14,7 @@ from PIL import Image
 import numpy as np
 
 
-def gaussian_kernel(size, sigma=1):
+def gaussian_kernel(size, sigma=1.0):
     size = int(size) // 2
     x, y = np.mgrid[-size:size + 1, -size:size + 1]
     normal = 1 / (2.0 * np.pi * sigma ** 2)
@@ -31,6 +31,14 @@ def convolve2d(image, kernel):
     # Returns:
     #   a numpy array of size [image_height, image_width] (convolution output).
 
+    # Get kernel shape
+    m, n = kernel.shape
+
+    # Set info of image
+    y, x = image.shape
+    y = y - m + 1
+    x = x - m + 1
+
     kernel = np.flipud(np.fliplr(kernel))  # Flip the kernel
     output = np.zeros_like(image)  # convolution output
     # Add zero padding to the input image
@@ -38,10 +46,10 @@ def convolve2d(image, kernel):
     image_padded = np.zeros((image.shape[0] + 2, image.shape[1] + 2))
     image_padded[1:-1, 1:-1] = image
 
-    for x in range(image.shape[1]):  # Loop over every pixel of the image
-        for y in range(image.shape[0]):
+    for i in range(y):  # Loop over every pixel of the image
+        for j in range(x):
             # element-wise multiplication of the kernel and the image
-            output[y, x] = (kernel * image_padded[y:y + 5, x:x + 5]).sum()
+            output[i, j] = np.sum(kernel * image_padded[i:i + m, j:j + m])
     return output
 
 
@@ -50,7 +58,7 @@ def main():
     logger.info("Starting Canny Edge Detector")
 
     # load the image
-    image = Image.open(os.path.join(sys.path[0], 'test-img.png'))
+    image = Image.open(os.path.join(sys.path[0], 'lena-colored.png'))
 
     # convert to greyscale
     image = image.convert('L')
