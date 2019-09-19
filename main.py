ae0c72a0276d50e3528bfa6f9d14a49d63a0d11b
Armend Ukehaxhaj <armendd.u@hotmail.com>
2019-09-19 17:20:01 +0200

    Added remaning functions for canny edge detector

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ __license__ = "MIT"
 from logzero import logger
 from PIL import Image
 import numpy as np
+from scipy import ndimage
 
 
 def gaussian_kernel(size, sigma=1.0):
@@ -53,6 +54,110 @@ def convolve2d(image, kernel):
     return output
 
 
+def sobel_filters(img):
+    Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
+    Ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
+
+    Ix = ndimage.filters.convolve(img, Kx)
+    Iy = ndimage.filters.convolve(img, Ky)
+
+    G = np.hypot(Ix, Iy)
+    G = G / G.max() * 255
+    theta = np.arctan2(Iy, Ix)
+
+    return G, theta
+
+
+def non_max_suppression(img, d):
+    M, N = img.shape
+    Z = np.zeros((M, N), dtype=np.int32)
+    angle = d * 180. / np.pi
+    angle[angle < 0] += 180
+
+    for i in range(1, M - 1):
+        for j in range(1, N - 1):
+            try:
+                q = 255
+                r = 255
+
+                # angle 0
+                if (0 <= angle[i, j] < 22.5) or (157.5 <= angle[i, j] <= 180):
+                    q = img[i, j + 1]
+                    r = img[i, j - 1]
+                # angle 45
+                elif 22.5 <= angle[i, j] < 67.5:
+                    q = img[i + 1, j - 1]
+                    r = img[i - 1, j + 1]
+                # angle 90
+                elif 67.5 <= angle[i, j] < 112.5:
+                    q = img[i + 1, j]
+                    r = img[i - 1, j]
+                # angle 135
+                elif 112.5 <= angle[i, j] < 157.5:
+                    q = img[i - 1, j - 1]
+                    r = img[i + 1, j + 1]
+
+                if (img[i, j] >= q) and (img[i, j] >= r):
+                    Z[i, j] = img[i, j]
+                else:
+                    Z[i, j] = 0
+
+            except IndexError as e:
+                pass
+
+    return Z
+
+
+def threshold(img):
+    weak_pixel = 75
+    strong_pixel = 255
+    lowth = 0.05
+    highth = 0.15
+
+    high_threshold = img.max() * highth;
+    lowThreshold = high_threshold * lowth;
+
+    M, N = img.shape
+    res = np.zeros((M, N), dtype=np.int32)
+
+    weak = np.int32(weak_pixel)
+    strong = np.int32(strong_pixel)
+
+    strong_i, strong_j = np.where(img >= high_threshold)
+    zeros_i, zeros_j = np.where(img < lowThreshold)
+
+    weak_i, weak_j = np.where((img <= high_threshold) & (img >= lowThreshold))
+
+    res[strong_i, strong_j] = strong
+    res[weak_i, weak_j] = weak
+
+    return res
+
+
+def hysteresis(img):
+    weak_pixel = 75
+    strong_pixel = 255
+    M, N = img.shape
+    weak = weak_pixel
+    strong = strong_pixel
+
+    for i in range(1, M - 1):
+        for j in range(1, N - 1):
+            if img[i, j] == weak:
+                try:
+                    if ((img[i + 1, j - 1] == strong) or (img[i + 1, j] == strong) or (img[i + 1, j + 1] == strong)
+                            or (img[i, j - 1] == strong) or (img[i, j + 1] == strong)
+                            or (img[i - 1, j - 1] == strong) or (img[i - 1, j] == strong) or (
+                                    img[i - 1, j + 1] == strong)):
+                        img[i, j] = strong
+                    else:
+                        img[i, j] = 0
+                except IndexError as e:
+                    pass
+
+    return img
+
+
 def main():
     """ Main entry point of the app """
     logger.info("Starting Canny Edge Detector")
@@ -76,10 +181,16 @@ def main():
 
     result = convolve2d(data, gaussian_mask)
 
-    # create Pillow image
-    image2 = Image.fromarray(result)
+    gradientMat, thetaMat = sobel_filters(result)
 
-    logger.info("Convolution done")
+    nonMaxImg = non_max_suppression(gradientMat, thetaMat)
+    thresholdImg = threshold(nonMaxImg)
+    img_final = hysteresis(thresholdImg)
+
+    # create Pillow image
+    image2 = Image.fromarray(img_final)
+
+    logger.info("Convolution done, opening image")
 
     image2.show()
 
